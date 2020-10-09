3587b4cbe831639d72207cb1f4c99c91fd3911fa
nguyenhaininh0306 <72057074+nguyenhaininh0306@users.noreply.github.com>
2020-10-09 19:47:56 +0700

    Add files via upload

---
+++ MyClient.java
@@ -0,0 +1,32 @@
+package Lab3;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class MyClient {
+
+	public static void main(String[] args) {
+		try {
+			MyClient client = new MyClient("LocalHost", 1500);
+			System.out.println("Client da duoc tao");
+			Scanner inFromServer = new Scanner(client.getInputStream());
+			PrintStream outToServer = new PrintStream(client.getOutputStream());
+			System.out.println("server: " + inFromServer.nextLine());
+			Scanner scan = new Scanner(System.in);
+			String ten = scan.nextLine();
+			outToServer.println(ten);
+			System.out.println("server: " + inFromServer.nextLine());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
